7e6efaffda525ee018f21d127cae991393f9a0b3
dariadau <qwertykoko12@gmail.com>
2025-02-25 19:59:13 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from .func3 import show_letter_sum
 from .func4 import find_num
 from .func5 import find_anagrams
 
-__all__ = (count_positives_and_negatives, find_max_and_min_list, show_letter_sum, find_num, find_anagrams)
+__all__ = ('count_positives_and_negatives', 'find_max_and_min_list', 'show_letter_sum', 'find_num', 'find_anagrams')
